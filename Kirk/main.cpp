0d62c183bcd091ea26873d97583f759e61052ded
Ben Snyder <johnbensnyder@gmail.com>
2019-10-07 12:08:51 -0700

    Initial Commit

---
+++ Kirk/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Kirk
+//
+//  Created by Ben Snyder on 10/7/19.
+//  Copyright © 2019 Ben Snyder. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
